d0f902656f6c60df88213b5467831a95b419fa6e
KEEKE132 <dksvlfdhs1@naver.com>
2024-10-09 19:26:52 +0900

    개발환경 세팅

---
+++ src/main/java/com/benchmalk/benchmalkServer/BenchmalkServerApplication.java
@@ -0,0 +1,13 @@
+package com.benchmalk.benchmalkServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmalkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchmalkServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benchmalk/benchmalkServer/BenchmalkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchmalk.benchmalkServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmalkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
