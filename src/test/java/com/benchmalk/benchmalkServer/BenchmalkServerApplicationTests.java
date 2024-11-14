7f6b801af16152d13dbe1468db9cdeba7676024b
KEEKE132 <dksvlfdhs1@naver.com>
2024-11-14 17:36:04 +0900

    test: 테스트 수정

--- src/test/java/com/benchmalk/benchmalkServer/BenchmalkServerApplicationTests.java
+++ src/test/java/com/benchmalk/benchmalkServer/BenchmalkServerApplicationTests.java
@@ -1,13 +1,11 @@
 package com.benchmalk.benchmalkServer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BenchmalkServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }
--- src/test/java/com/benchmalk/benchmalkServer/user/repository/UserRepositoryTest.java
+++ src/test/java/com/benchmalk/benchmalkServer/user/repository/UserRepositoryTest.java
@@ -1,25 +1,12 @@
 package com.benchmalk.benchmalkServer.user.repository;
 
-import com.benchmalk.benchmalkServer.user.domain.User;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
-
-    @Test
-    void findById() {
-        User user = new User("id1", "user1", "123");
-        userRepository.save(user);
-
-        assertThat(userRepository.findById(user.getId()).get()).isEqualTo(user);
-        assertThat(userRepository.findByUsername(user.getUsername()).get()).isEqualTo(user);
-    }
 }
