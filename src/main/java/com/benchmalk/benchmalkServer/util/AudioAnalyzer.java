e0a20fc725c88ddeef8659a509c83babab9d6e89
KEEKE132 <dksvlfdhs1@naver.com>
2024-11-21 16:31:45 +0900

    feat: Pitch 분석 추가
    MP3SPI,TarsosDSP 종속성 사용

--- src/main/java/com/benchmalk/benchmalkServer/util/AudioAnalyzer.java
+++ src/main/java/com/benchmalk/benchmalkServer/util/AudioAnalyzer.java
@@ -1,10 +1,8 @@
 package com.benchmalk.benchmalkServer.util;
 
 import be.tarsos.dsp.AudioDispatcher;
-import be.tarsos.dsp.AudioEvent;
-import be.tarsos.dsp.io.jvm.JVMAudioInputStream;
+import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
 import be.tarsos.dsp.pitch.PitchDetectionHandler;
-import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.PitchProcessor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,37 +12,58 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 @RequiredArgsConstructor
 public class AudioAnalyzer {
-    public void test() {
-        String homePath = System.getProperty("user.home");
-        String audioFilePath = homePath + "/benchmalk/files/test.mp3";
+    public void analyzePitch(String filePath) {
 
+        AudioDispatcher dispatcher;
         try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(audioFilePath));
-            JVMAudioInputStream jvmAudioStream = new JVMAudioInputStream(audioStream);
+            // Increase buffer size and add overlap
+            int bufferSize = 4096;
+            int overlap = 1024;
 
-            AudioDispatcher dispatcher = new AudioDispatcher(jvmAudioStream, 1024, 0);
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(filePath));
 
-            PitchDetectionHandler pitchHandler = new PitchDetectionHandler() {
-                @Override
-                public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
-                    float pitchInHz = pitchDetectionResult.getPitch();
-                    System.out.println(audioEvent.getTimeStamp() + ": Detected pitch: " + pitchInHz + " Hz");
-                }
+            int sampleRate = (int) inputStream.getFormat().getSampleRate();
+
+            System.out.println(inputStream.getFormat().toString());
+
+            dispatcher = AudioDispatcherFactory.fromPipe(filePath, sampleRate, bufferSize, overlap);
+
+            if (dispatcher == null) {
+                System.out.println("Dispatcher 생성 실패");
+                return;
+            }
+
+            ArrayList<Float> result = new ArrayList<>();
+
+            PitchDetectionHandler pitchHandler = (pitchDetectionResult, audioEvent) -> {
+                float pitchInHz = pitchDetectionResult.getPitch();
+                System.out.println(audioEvent.getTimeStamp() + ": Detected pitch: " + pitchInHz + " Hz");
+                result.add(pitchInHz);
             };
 
-            PitchProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.YIN, 44100, 1024, pitchHandler);
+            PitchProcessor pitchProcessor = new PitchProcessor(
+                    PitchProcessor.PitchEstimationAlgorithm.YIN,
+                    sampleRate,
+                    bufferSize,
+                    pitchHandler
+            );
+
             dispatcher.addAudioProcessor(pitchProcessor);
 
             dispatcher.run();
 
         } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
+            System.out.println("오디오 파일 형식 지원되지 않음: " + e.getMessage());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("파일 읽기 실패: " + e.getMessage());
+        } catch (NegativeArraySizeException e) {
+            System.out.println("NegativeArraySizeException 발생: 버퍼 설정 또는 샘플 레이트 확인 필요");
+            e.printStackTrace();
         }
     }
 }
--- src/test/java/com/benchmalk/benchmalkServer/util/AudioAnalyzerTest.java
+++ src/test/java/com/benchmalk/benchmalkServer/util/AudioAnalyzerTest.java
@@ -1,13 +1,33 @@
 package com.benchmalk.benchmalkServer.util;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
 
 class AudioAnalyzerTest {
 
     @Test
     void test() {
         AudioAnalyzer a = new AudioAnalyzer();
-        a.test();
+        Assertions.assertThatCode(() ->
+                a.analyzePitch("src/test/resources/test.mp3")).doesNotThrowAnyException();
+    }
+
+    @Test
+    void test2() throws UnsupportedAudioFileException, IOException {
+        Assertions.assertThatCode(() -> {
+            String audioFilePath = "src/test/resources/test.mp3";
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new File(audioFilePath));
+            AudioFormat format = ais.getFormat();
+            System.out.println(format.toString());
+        }).doesNotThrowAnyException();
+
     }
 
 }
