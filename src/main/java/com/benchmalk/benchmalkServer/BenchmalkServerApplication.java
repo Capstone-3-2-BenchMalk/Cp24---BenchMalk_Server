99f79d9c3787cced8792031ae216d04d27a3b2ca
KEEKE132 <dksvlfdhs1@naver.com>
2024-12-05 23:10:16 +0900

    fix: 서버 Timezone 수정

--- src/main/java/com/benchmalk/benchmalkServer/BenchmalkServerApplication.java
+++ src/main/java/com/benchmalk/benchmalkServer/BenchmalkServerApplication.java
@@ -3,10 +3,13 @@ package com.benchmalk.benchmalkServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BenchmalkServerApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(BenchmalkServerApplication.class, args);
     }
 
