de0dc5a6ed63bf163a9ea8b5847d710b7e1bd712
KEEKE132 <dksvlfdhs1@naver.com>
2024-12-08 17:37:04 +0900

    fix: SD측정시 2Sigma 필터링

--- src/test/java/com/benchmalk/benchmalkServer/util/ScoreCalculatorTest.java
+++ src/test/java/com/benchmalk/benchmalkServer/util/ScoreCalculatorTest.java
@@ -1,11 +1,19 @@
 package com.benchmalk.benchmalkServer.util;
 
 import com.benchmalk.benchmalkServer.clova.repository.ClovaAnalysisRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class ScoreCalculatorTest {
     private final ScoreCalculator calculator = new ScoreCalculator();
     private final AudioAnalyzer analyzer = new AudioAnalyzer();
     private ClovaAnalysisRepository clovaAnalysisRepository;
+
+    @Test
+    void test() {
+        Assertions.assertThatCode(() ->
+                System.out.println(calculator.analyzeSD(analyzer.analyzePitch("src/test/resources/byung.mp3")))).doesNotThrowAnyException();
+    }
 
 
 }
